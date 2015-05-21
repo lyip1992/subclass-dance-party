b1ced4ef64d9f2efd1d5d2f211abec331f38d61d
lyip1992 <lyip1992@gmail.com>
2015-05-20 20:34:37 -0700

    Added additional tests and styles for two new dancers

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -1,3 +1,39 @@
+describe("runningDancer", function() {
+
+  var runningDancer;
+  var timeBetweenSteps = 100;
+  var clock;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    runningDancer = new RunningDancer(10, 20, timeBetweenSteps);
+  });
+
+  it("should have a jQuery $node object", function(){
+    expect(runningDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it("should have a step function that makes its node blink", function() {
+    sinon.spy(runningDancer.$node, 'toggle');
+    runningDancer.step();
+    expect(runningDancer.$node.toggle.called).to.be.true;
+  });
+
+  describe("dance", function(){
+    it("should call step at least once per second", function(){
+      sinon.spy(runningDancer, "step");
+      expect(runningDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(runningDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(runningDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
+
 describe("blinkyDancer", function() {
 
   var blinkyDancer;
@@ -30,6 +66,42 @@ describe("blinkyDancer", function() {
 
       clock.tick(timeBetweenSteps);
       expect(blinkyDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
+
+describe("crawlingDancer", function() {
+
+  var crawlingDancer;
+  var timeBetweenSteps = 100;
+  var clock;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    crawlingDancer = new CrawlingDancer(10, 20, timeBetweenSteps);
+  });
+
+  it("should have a jQuery $node object", function(){
+    expect(crawlingDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it("should have a step function that makes its node blink", function() {
+    sinon.spy(crawlingDancer.$node, 'toggle');
+    crawlingDancer.step();
+    expect(crawlingDancer.$node.toggle.called).to.be.true;
+  });
+
+  describe("dance", function(){
+    it("should call step at least once per second", function(){
+      sinon.spy(crawlingDancer, "step");
+      expect(crawlingDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(crawlingDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(crawlingDancer.step.callCount).to.be.equal(2);
     });
   });
 });
